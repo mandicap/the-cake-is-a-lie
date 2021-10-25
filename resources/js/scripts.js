03a91cbcc9ee49d26e277a40394126ce3361cdee
Amanda Romero <mandicap@comcast.net>
2021-10-25 04:31:25 -0600

    Prepares for styling and functionality

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,2 +1,10 @@
 import '../css/styles.css';
+
 import Alpine from 'alpinejs';
+
+// Register any extension code here
+// ex: Alpine.plugin(collapse)
+
+window.Alpine = Alpine;
+
+Alpine.start();
--- templates/layout/default.php
+++ templates/layout/default.php
@@ -1,42 +1,35 @@
-<?php
-/**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         0.10.0
- * @license       https://opensource.org/licenses/mit-license.php MIT License
- * @var \App\View\AppView $this
- */
-
-$cakeDescription = 'CakePHP: the rapid development php framework';
-?>
 <!DOCTYPE html>
-<html><head>
+<html lang="en"><head>
 
     <?= $this->Html->charset() ?>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title><?= $cakeDescription ?>: <?= $this->fetch('title') ?></title>
-
-    <?= $this->Html->meta('icon') ?>
-
-    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Raleway:400,700">
-    <?= $this->Html->css('styles.css') ?>
+    <title><?= $this->fetch('title') ?></title>
 
     <?= $this->fetch('meta') ?>
+
+    <?= $this->Html->meta('icon') ?>
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Raleway:400,700">
+    <?= $this->Html->css('styles.css') ?>
     <?= $this->fetch('css') ?>
-    <?= $this->fetch('script') ?>
 
 </head><body>
 
-    <?= $this->fetch('content') ?>
+    <div id="app">
+        <header class="p-4">
+            Header
+        </header>
 
-    <?= $this->Html->script('scripts') ?>
+        <main class="flex-grow flex flex-col justify-center min-h-full">
+            <?= $this->fetch('content') ?>
+        </main>
+
+        <footer class="p-4">
+            Footer
+        </footer>
+    </div>
+
+    <?= $this->Html->script('scripts.js') ?>
+    <?= $this->fetch('script') ?>
 
 </body></html>
