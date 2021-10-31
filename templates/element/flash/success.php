a25329ef7a312a670b0a2169f95651a6ce4b9a9f
Amanda Romero <mandicap@comcast.net>
2021-10-31 01:35:38 -0600

    Styles flash messages

--- templates/element/flash/success.php
+++ templates/element/flash/success.php
@@ -1,11 +1,9 @@
-<?php
-/**
- * @var \App\View\AppView $this
- * @var array $params
- * @var string $message
- */
-if (!isset($params['escape']) || $params['escape'] !== false) {
-    $message = h($message);
-}
-?>
-<div class="message success" onclick="this.classList.add('hidden')"><?= $message ?></div>
+<div x-data class="relative mb-8 p-4 w-full bg-green-500 text-white">
+    <p><?= h($message) ?></p>
+
+    <button class="absolute top-1/2 right-0 mr-4 -mt-2" @click="$root.remove()">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+        </svg>
+    </button>
+</div>
