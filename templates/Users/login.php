40b0a834dccfba0fdc6d74ca193f16a07a0f376c
Amanda Romero <mandicap@comcast.net>
2021-11-15 10:59:24 -0700

    Adds @tailwindcss/forms

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -3,15 +3,17 @@
 <?= $this->Form->create() ?>
     <div class="space-y-6">
         <?= $this->Form->control('email', [
-            'class' => 'appearance-none block w-full mt-1 px-3 py-2 border border-gray-300 rounded-sm shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm',
+            'class' => 'mt-0 block w-full px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-black',
             'required' => true,
-            'label' => ['class' => 'block text-sm font-medium text-gray-700']
+            'placeholder' => __('Email'),
+            'label' => ['class' => 'sr-only']
         ]) ?>
 
         <?= $this->Form->control('password', [
-            'class' => 'appearance-none block w-full mt-1 px-3 py-2 border border-gray-300 rounded-sm shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm',
+            'class' => 'mt-0 block w-full px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-black',
             'required' => true,
-            'label' => ['class' => 'block text-sm font-medium text-gray-700']
+            'placeholder' => __('Password'),
+            'label' => ['class' => 'sr-only']
         ]) ?>
 
         <?= $this->Form->submit(__('Login'), ['class' => 'btn xl secondary w-full justify-center']); ?>
